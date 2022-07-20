5c6ddfa73c479fd8c9db44ca86b15101d912ceed
Blue Jessen <canealisblue@gmail.com>
2022-07-20 08:39:24 -0700

    Set up test to visit and intercept API call

---
+++ cypress/e2e/App.cy.js
@@ -0,0 +1,11 @@
+describe('empty spec', () => {
+  beforeEach('Visit home page', () => {
+    cy.visit('localhost:3000')
+    cy.intercept('http://localhost:3001/api/v1/reservations', {fixture: 'reservations'})
+  })
+
+  it('Should display all reservations', () => {
+    cy.get('.reservation-card').eq(1).contains('Leta')
+    cy.get('.reservation-card').should('have.length', 4)
+  })
+})
