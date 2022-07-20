33f20d1033b6a2765fb75599fa5f184bf2bda905
Blue Jessen <canealisblue@gmail.com>
2022-07-20 09:43:28 -0700

    Add reservation form file

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,13 +1,13 @@
 describe('empty spec', () => {
   beforeEach('Visit home page', () => {
-    cy.visit('localhost:3000')
     cy.intercept('http://localhost:3001/api/v1/reservations', {fixture: 'reservations'})
+    cy.visit('localhost:3000')
   })
 
   it('Should display all reservations', () => {
     cy.get('.reservation-card').eq(1).contains('Leta')
     cy.get('.reservation-card').should('have.length', 4)
-  })  //iteration 0
+  })  //iteration 1
 
   it('Should have a reservation form', () => {
     cy.get('form').within(() => {
@@ -38,5 +38,6 @@ describe('empty spec', () => {
     cy.get('form').within(() => {
       cy.get('input').eq(3).type(4)
       cy.get('button').click()
-  }
+  })//iteration 2
+})
 })
